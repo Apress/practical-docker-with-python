242ce3fac24f596a399dd2ce5aa2ce08aa0bc4c3
SathyaBhat <sathya@sathyasays.com>
2018-11-26 23:44:17 +0530

    bug fixes to prevent bot from crashing
    when trying to fetch data w/o setting the source

--- subredditfetcher_bot_volumes/states.py
+++ subredditfetcher_bot_volumes/states.py
@@ -4,7 +4,7 @@ import logging
 class States(object):
     last_updated_id = ''
 
-logging.basicConfig(level=logging.INFO,
+logging.basicConfig(level=logging.DEBUG,
                     format='%(levelname)s: %(asctime)s - %(funcName)s - %(message)s')
 
 log = logging.getLogger('nbt')
